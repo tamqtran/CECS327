1a70a48493e0b40f3c47d26307552a8557b4c362
DuongPham14 <31968090+DuongPham14@users.noreply.github.com>
2018-09-07 13:31:55 -0700

    Json Format
    The json file should be in the project folder.

--- JSONTEST.java
+++ JSONTEST.java
@@ -11,37 +11,71 @@ public class JSONTEST {
 
 	public static void main(String[] args) {
 		//Writing to JSON
-		/*
+		
 		//Create JSON object and add value
 		JSONObject obj = new JSONObject();
-		obj.put("name", "Allan");
-		obj.put("password", "Forever");
+		obj.put("name", "allan");
+		obj.put("password", "forever");
 		
 		//Create JSON array and add value
-		JSONArray list = new JSONArray();
-		list.put("playlist1");
-		list.put("playlist2");
-		list.put("playlist3");
+		JSONArray playlists = new JSONArray();
+		playlists.put("playlist1");
+		playlists.put("playlist2");
+		playlists.put("playlist3");
 		
-		obj.put("playlists", list);
+		obj.put("playlists", playlists);
+		
+		JSONArray songlists = new JSONArray();
+		songlists.put("song1");
+		songlists.put("song2");
+		songlists.put("song3");
+		
+		obj.put("playlist1", songlists);
+		obj.put("playlist2", songlists);
+		obj.put("playlist3", songlists);
 		
 		//Write all to json file
 		try {
-			FileWriter fileWriter = new FileWriter("Allan.json");
+			FileWriter fileWriter = new FileWriter("allan.json");
 			fileWriter.write(obj.toString());
 			fileWriter.flush();
 			fileWriter.close();
 		}catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 		
 		//Reading from JSON
-		JSONObject obj;
-		String pathname = "practice.json";
+		JSONObject obj1;
+		String pathname = "allan.json";
 		try (InputStream input = new FileInputStream(pathname)) {
-		    obj = new JSONObject(new JSONTokener(input));
-		    String name = obj.get("playlist").toString();
-		    System.out.println(name);
+		    obj1 = new JSONObject(new JSONTokener(input));
+		    //read name + password
+		    String name = obj1.get("name").toString();
+		    String password = obj1.get("password").toString();
+		    System.out.println("Name: "+ name+"\nPassword: "+password);
+		    
+		    System.out.println();
+		    
+		    //read playlists
+		    String playlist = obj1.get("playlists").toString();
+		    String [] playlistArray = playlist.substring(2, playlist.length() - 2).split("\",\"");
+		    System.out.print("Playlists: ");
+		    for(int i = 0; i < playlistArray.length; i++)
+		    	System.out.print(playlistArray[i] + " ");
+		    
+		    System.out.println();
+		    
+		    //songs of each playlist
+		    for(int i = 0; i < playlistArray.length; i++) {
+		    	String songlist = obj1.get(playlistArray[i]).toString();
+		    	String[] songlistArray = songlist.substring(2,songlist.length()-2).split("\",\"");
+		    	System.out.println();
+		    	
+		    	System.out.print(playlistArray[i] + ": ");
+		    	for(int j  = 0; j < songlistArray.length; j++) 
+		    		System.out.print(songlistArray[j] + " ");
+		    }
+		    
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
