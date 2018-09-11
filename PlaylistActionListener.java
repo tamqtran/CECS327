491a1c6d08cd56b149824204044e740310e3ca41
tamqtran <tamtran1997@gmail.com>
2018-09-11 00:07:54 -0700

    almost done with playlist GUI need to have selected song be saved as a variable need to finish action listeners
    discussion next meeting:
    -how to pass arguments
    -talk with Vincent on how to add songs in the playlist
    -dispose Frames talk

--- PlaylistActionListener.java
+++ PlaylistActionListener.java
@@ -1,15 +1,25 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+
+import javax.swing.JFrame;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class PlaylistActionListener implements ActionListener
 {
 	private PlaylistPanel panel;
 	
-	public PlaylistActionListener(PlaylistPanel p)
-	{
+	public PlaylistActionListener(PlaylistPanel p) 
+	{ 
 		this.panel = p;
 	}
-		
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
@@ -17,11 +27,16 @@ public class PlaylistActionListener implements ActionListener
 		
 		if (source == panel.backToProfileButton)
 		{
-			
+			JFrame pFrame = (JFrame) panel.getTopLevelAncestor();
+			pFrame.dispose();
+			new Profile().setVisible(true);
+	        
 		}
 		else if(source == panel.selectSongButton)
 		{
-			
+			JFrame pFrame = (JFrame) panel.getTopLevelAncestor();
+			pFrame.dispose();
+			//new PlayButton().setVisible(true);
 		}
 		else if(source == panel.addSongButton)
 		{
@@ -30,7 +45,30 @@ public class PlaylistActionListener implements ActionListener
 		else if(source == panel.deleteSongButton)
 		{
 			
+			String username = panel.username;
+			String playlistName = panel.playlist;
+			String songName;
+			
+			try (InputStream input = new FileInputStream(username+".json")) 
+			{
+				// read as JSON file
+			    JSONObject JSONfile = new JSONObject(new JSONTokener(input));
+			    
+			    // get correct playlist from JSON file
+			    JSONArray pJSON = JSONfile.getJSONArray(playlistName);//play
+			    
+			    //pJSON.remove(pJSON.toList().indexOf(songName));
+			 
+			    FileWriter fileWriter = new FileWriter(username+".json");
+				fileWriter.write(JSONfile.toString());
+				fileWriter.flush();
+				fileWriter.close();
+			    
+			}
+			catch (Exception f) 
+			{
+				f.printStackTrace();
+			}
 		}
 	}
-
 }
--- PlaylistPanel.java
+++ PlaylistPanel.java
@@ -1,11 +1,19 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class PlaylistPanel extends JPanel
 {
@@ -18,20 +26,29 @@ public class PlaylistPanel extends JPanel
 	
 	public JList songList;
 	
+	String username,
+		   playlist;
 	// based on absolute layout to organize the components in the frame (https://docs.oracle.com/javase/tutorial/uiswing/layout/none.html)
 	
-	public PlaylistPanel()
+	public PlaylistPanel(String username, String playlist)
 	{
+		this.username = username;
+		this.playlist = playlist;
+		
+		
+		ActionListener listener = new PlaylistActionListener(this);
+		
 		this.setLayout(null);
 		
 		// back to profile button
 		backToProfileButton = new JButton("Back to Profile");
 		backToProfileButton.setSize(backToProfileButton.getPreferredSize());
 		backToProfileButton.setLocation(10, 10);
+		backToProfileButton.addActionListener(listener);
 		this.add(backToProfileButton);
 		
 		// playlist name label
-		playlistName = new JLabel("Playlist Name: ");
+		playlistName = new JLabel("Playlist Name: " + playlist);
 		playlistName.setSize(playlistName.getPreferredSize());
 		playlistName.setLocation(50, 100);
 		this.add(playlistName);
@@ -40,36 +57,52 @@ public class PlaylistPanel extends JPanel
 		selectSongButton = new JButton("Select Song");
 		selectSongButton.setSize(selectSongButton.getPreferredSize());
 		selectSongButton.setLocation(350, 500);
+		selectSongButton.addActionListener(listener);
 		this.add(selectSongButton);
 		
 		// add song button 
 		addSongButton = new JButton("Add Song");
 		addSongButton.setSize(addSongButton.getPreferredSize());
 		addSongButton.setLocation(457, 500);
+		addSongButton.addActionListener(listener);
 		this.add(addSongButton);
 		
 		// delete song button
 		deleteSongButton = new JButton("Delete Song");
 		deleteSongButton.setSize(deleteSongButton.getPreferredSize());
 		deleteSongButton.setLocation(550, 500);
+		deleteSongButton.addActionListener(listener);
 		this.add(deleteSongButton);
 		
 		// list of songs in the playlist
 		songList = new JList();
 		songList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		DefaultListModel DM = new DefaultListModel();
+		songList.setModel(DM);
 		songList.setSize(300,380);
 		songList.setLocation(350, 100);
-		this.add(songList);
+		this.add(songList);		
 		
-		backToProfileButton.addActionListener(new ActionListener() 
+		// add content to songList
+					
+		
+		try (InputStream input = new FileInputStream(username+".json")) 
 		{
-			public void actionPerformed(ActionEvent e) 
-			{
-				//frame.remove();
-				new Login().setVisible(true); 
-			}
-		});
-												
+		    JSONObject obj = new JSONObject(new JSONTokener(input));
+		    
+		    JSONArray listOfSongs = obj.getJSONArray(playlist);
+		    
+		    for(int i = 0; i < listOfSongs.length(); i++)
+		    {
+		    	DM.addElement(listOfSongs.getString(i));
+		    }
+		    //currentList.remove(currentList.toList().indexOf(playlist)); 
+		    
+		    
+		}catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
