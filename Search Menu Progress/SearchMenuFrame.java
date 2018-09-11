4c7369ba87f209532a33829c97025263ecf25dfa
tamqtran <tamtran1997@gmail.com>
2018-09-11 11:01:46 -0700

    Add files via upload
    SearchInFolder- just a test file. ignore.
    searchmenupanel has everything.
    Currently, it works with text files. There is a variable for extension type that we can change.

--- Search Menu Progress/SearchMenuFrame.java
+++ Search Menu Progress/SearchMenuFrame.java
@@ -6,7 +6,7 @@ public class SearchMenuFrame extends JFrame {
   public SearchMenuFrame() {
     setTitle("Song Search");
     setResizable(false);
-    setSize(300,400);
+    setSize(500,400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     JPanel panel = new SearchMenuPanel();
     this.add(panel);
