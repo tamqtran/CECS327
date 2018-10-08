469172d638ff303c29142dad258ee5be52abf35f
chanito2 <fett22@rocketmail.com>
2018-10-07 21:44:23 -0700

    Update PlayButton.java

--- PlayButton.java
+++ PlayButton.java
@@ -178,54 +178,53 @@ public class PlayButton {
 				}
 			}
 		}
-		class XActionListener implements ActionListener{//
+		class XActionListener implements ActionListener
+		{
 			private PlayPanel panel;
 			Clip current;
 			int pos = 0;
-			
+			int songIndex = 0;
+
 			String username = uName;
 			ArrayList<String> songList = new ArrayList<String>();
-			int songIndex = songList.indexOf(song);
-			
-			public void updatePlaylist() 
 			{
-				
-				try (InputStream input = new FileInputStream(username+".json"))
-				{
-				
-					JSONObject obj = new JSONObject(new JSONTokener(input));								// turn into JSON object
-				    
-				    JSONArray listOfSongs = obj.getJSONArray(playlist);										// grabs JSON array of songs by mapping the playlist name
-				    
-				    for(int j = 0; j < listOfSongs.length(); j++)											// adds all songs from array into JList
-				    {
-				    	String temp = listOfSongs.getString(j);
-				    	String[] transferSong = SearchMenuPanel.search(temp);
-						
-						// get the list of .wav files and separate by song, artist, and album
-						String[] column = { "Song Title", "Artist", "Album" };
-						DefaultTableModel model = new DefaultTableModel(null, column);
-						model.setRowCount(0);
-						for (int i = 0; i < transferSong.length; i++) {
-							model.addRow(transferSong[i].split("_"));
-						};
-						
-						// get selected song variables
-						String songTitle = model.getValueAt(0, 0).toString();
-						String artist = model.getValueAt(0, 1).toString();
-						String album = model.getValueAt(0, 2).toString();
-						
-						//change text on labels in homepage
-				    	songList.add(songTitle + "_" + artist + "_" + album);
-				    }
+			
+			try (InputStream input = new FileInputStream(username+".json"))
+			{
 
-				}
-				catch (Exception e) 																		// catch exception
+				JSONObject obj = new JSONObject(new JSONTokener(input));								// turn into JSON object
+
+				JSONArray listOfSongs = obj.getJSONArray(playlist);										// grabs JSON array of songs by mapping the playlist name
+
+				for(int j = 0; j < listOfSongs.length(); j++)											// adds all songs from array into JList
 				{
-					e.printStackTrace();
-					
+					String temp = listOfSongs.getString(j);
+					String[] transferSong = SearchMenuPanel.search(temp);
+
+					// get the list of .wav files and separate by song, artist, and album
+					String[] column = { "Song Title", "Artist", "Album" };
+					DefaultTableModel model = new DefaultTableModel(null, column);
+					model.setRowCount(0);
+					for (int i = 0; i < transferSong.length; i++) {
+						model.addRow(transferSong[i].split("_"));
+					};
+
+					// get selected song variables
+					String songTitle = model.getValueAt(0, 0).toString();
+					String artist = model.getValueAt(0, 1).toString();
+					String album = model.getValueAt(0, 2).toString();
+
+					//change text on labels in homepage
+					songList.add(songTitle + "_" + artist + "_" + album);
 				}
+				int songIndex = songList.indexOf(song);
+
 			}
+			catch (Exception e) 																		// catch exception
+			{
+				e.printStackTrace();
+
+			}}
 
 			public XActionListener(PlayPanel b)
 			{
