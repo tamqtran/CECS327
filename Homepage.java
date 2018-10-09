0aa6c59aa73503ad833c46b903c100ac333b41ec
MrAlphalfa97 <32249785+MrAlphalfa97@users.noreply.github.com>
2018-10-08 21:40:30 -0700

    just re-adding this codeDenial back in
    the search query needs it

--- Homepage.java
+++ Homepage.java
@@ -319,8 +319,13 @@ public class Homepage
 						System.out.println("Searching for: " + searchField.getText());
 						System.out.println("Current panel in ShiftingPanel is " + ShiftingPanel.getCurrentPanelName());
 						
+						if (!codeDenial(searchField.getText())) // if special characters are used then this will go off
+						{
+							JOptionPane.showMessageDialog(frame, "You can't do that. Stop it!", "Inane warning", JOptionPane.WARNING_MESSAGE);
+							searchField.setText("");
+						}
 						// checks if the current panel is the same one as the one that just got clicked
-						if (!searchField.getText().equals(ShiftingPanel.getCurrentPanelName())) 
+						else if (!searchField.getText().equals(ShiftingPanel.getCurrentPanelName())) 
 						{
 							SearchMenuPanel newPanel = new SearchMenuPanel(userName, getSearchResults(searchField.getText()),searchField.getText());
 							playlist = "x";
