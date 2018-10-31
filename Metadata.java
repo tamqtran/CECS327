171ba9dd8e37b9e69d19d2d6415f540100b33422
MrAlphalfa97 <32249785+MrAlphalfa97@users.noreply.github.com>
2018-10-30 19:16:06 -0700

    what I think his notes are saying...
    just use this (and change as you need to)

--- Metadata.java
+++ Metadata.java
@@ -1,22 +1,62 @@
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 class Chunk {
-	File f;
+	int guid;
+	String first, last;
+	byte[] content; // ????
+	public Chunk(int guid) {this.guid = guid;}
+	
+	//ls, touch, ))
+}
+
+class File {
 	String filename;
+	List<Chunk> chunks;
+	
+	public byte[] getChunk(int i) {
+		Chunk c = chunks.get(i);
+		return c.content;
+	}
+	
+	public File(String fn) {
+		filename = fn;
+	}
+	
+	public String getFilename() {return filename;}
+	
+	public void append(byte[] content) {
+		int guid = getHash(content); //change this to whatever we need
+		Chunk c = new Chunk(guid);
+		chunks.add(c);
+		peer[0].put(guid, content);
+		
+		metadata.append("inverted_index", content);
+	}
 }
 
 public class Metadata {
-	List<?> list;
-	String first, last;
+	List<File> fileList;
 	
-	public Metadata() {
-		list = new ArrayList<>();
+	public Metadata() {}
+	
+	public void append(String filename, byte[] content) {
+		File f;
+		f = matchFile(filename);
+
+		if (f == null)
+			f = new File(filename);
+		
+		f.append(content);
 	}
 	
-//	append
-//	delete
-//	read
-//	ls
+	private File matchFile(String filename) {
+		File F = null;
+		for (File f : fileList) {
+			if (filename.equals(f.getFilename()))
+				F = f;
+		}
+		return F;
+	}
 }
+
