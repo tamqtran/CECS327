4210a31504bcb75fda0bedb8978d85917d4c1851
tamqtran <tamtran1997@gmail.com>
2018-11-01 00:30:47 -0700

    run refreshJSON.java to get METADATA.json file then refresh your heirarchy to see the new file

--- refreshJSON.java
+++ refreshJSON.java
@@ -110,7 +110,69 @@ public class refreshJSON
 		System.out.println(allan);
 		System.out.println(bill);
 		System.out.println(tony);
-	}
 	
+		
+		// testing METADATA
+		JSONObject METADATA = new JSONObject();
+		JSONArray chunks = new JSONArray();
+		
+		JSONObject chunk1 = new JSONObject();	// songtitle that starts from letter a to i
+		JSONObject chunk2 = new JSONObject();	// songtitle that starts from letter j to q
+		JSONObject chunk3 = new JSONObject();	// songtitle that starts from letter r to z
+		
+		JSONArray chunkList1 = new JSONArray();
+		JSONArray chunkList2 = new JSONArray();
+		JSONArray chunkList3 = new JSONArray();
+		
+		JSONObject song1 = new JSONObject();
+		song1.put("filename","Hello Goodbye_The Beatles_Magical Mystery Tour.wav");
+		song1.put("guid", "1");
+		
+		JSONObject song2 = new JSONObject();
+		song2.put("filename","Money_Pink Floyd_The Dark Side of the Moon.wav");
+		song2.put("guid", "2");
+		
+		JSONObject song3 = new JSONObject();
+		song3.put("filename","Shadows_Jilian Aversa_Origins.wav");
+		song3.put("guid", "3");
+		
+		JSONObject song4 = new JSONObject();
+		song4.put("filename","So Serious_Electric Light Orchestra_Balance of Power.wav");
+		song4.put("guid", "4");
+		
+		JSONObject song5 = new JSONObject();
+		song5.put("filename","Yellow Submarine_The Beatles_Yellow Submarine.wav");
+		song5.put("guid", "5");
+		
+		chunkList1.put(song1);
+		chunkList2.put(song2);
+		chunkList3.put(song3);
+		chunkList3.put(song4);
+		chunkList3.put(song5);
+		
+		chunk1.put("chunk", chunkList1);
+		chunk2.put("chunk", chunkList2);
+		chunk3.put("chunk", chunkList3);
+		
+		chunks.put(chunk1);
+		chunks.put(chunk2);
+		chunks.put(chunk3);
+		
+		METADATA.put("chunks", chunks);
+		
+		
+		try 											// Write into json file
+		{
+			FileWriter fileWriter = new FileWriter("METADATA.json");
+			fileWriter.write(METADATA.toString());
+			fileWriter.flush();
+			fileWriter.close();
+		}catch (Exception e) 							// catch exception
+		{
+			e.printStackTrace();
+		}
+		
+		System.out.println(METADATA);
+	}
 
 }
