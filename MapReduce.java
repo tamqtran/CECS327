9534efb517d72cf8a42db6f91556e197db9cd9b0
Duong Pham <31968090+DuongPham14@users.noreply.github.com>
2018-11-27 11:35:01 -0800

    s

--- MapReduce.java
+++ MapReduce.java
@@ -1,6 +1,7 @@
 import net.tomp2p.dht.PeerDHT;
 
 public class MapReduce {
+	
 	public static void emitReduce(PeerDHT peer,String key, String value, Counter counter) {
 		
 	}
@@ -19,5 +20,27 @@ public class MapReduce {
 	
 	public static void completed(PeerDHT peer, Integer source, Counter counter) {
 		
+	}
+	
+	public static void runMapReduce(String file) {
+		/* Pseudo code from assignment 4 
+		 * 
+		mapCounter = new Counter();
+		reduceCounter = new Counter();
+		completedCounter = new Counter();
+		mapper = new MapInterface(); 
+		reducer = new ReduceInterface(); 
+		// map Phases 
+		for each page in metafile.file 
+			mapCounter.add(page); 
+			let peer be the process responsible for storing page 
+			peer.mapContext(page, mapper, mapCounter) 
+			wait until mapCounter.hasCompleted() = true 
+			// reduce phase 
+			reduceContext(guid, reducer, reduceCounter); 
+			wait until reduceCounter.hasCompleted() = true; 
+			completed(guid, completedCounter); 
+			wait until completedCounter.hasCompleted() = true;
+	*/
 	}
 }
